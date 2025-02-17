5639ab7e1d3f350568355ba25c2239ef0e61e1ae
Amir zekry <amirzekry43@gmail.com>
2025-02-17 04:27:01 +0200

    website adjustments

--- ui/shoppingCartProducts.jsx
+++ ui/shoppingCartProducts.jsx
@@ -4,8 +4,9 @@ import Image from 'next/image';
 import React from 'react';
 import { TbShoppingCartCancel } from "react-icons/tb";
 import { useDispatch, useSelector } from 'react-redux';
-import { setLoading } from '@/Redux/cartProduct';
 import { quantityDecrement, quantityIncrement, removeProduct } from '@/app/actions';
+import { setLoading } from '@/Redux/cartProduct';
+import { useState } from 'react';
 
 const ShoppingCartProducts = ({ product }) => {
     const dispatch = useDispatch();
@@ -21,9 +22,50 @@ const ShoppingCartProducts = ({ product }) => {
         }
     };
 
+    const [Loading, setIsLoading] = useState(false);
+
+    const quantityMinus = async () => {
+        if (product.product_quantity <= 1) return;
+        setIsLoading(true)
+        await quantityDecrement(product.product_id)
+        setIsLoading(false);
+    };
+
+    const quantityPlus = async () => {
+        setIsLoading(true);
+        await quantityIncrement(product.product_id)
+        setIsLoading(false);
+    };
+
+
 
     return (
-        <div className="flex flex-col h-1/3 border-t items-center justify-center pb-2">
+        <div className={`flex relative flex-col h-1/3 border-t items-center justify-center pb-2 ${Loading ? 'text-gray-200' : ''}`}>
+            {Loading && (
+                <svg
+                    className="absolute animate-spin h-10 w-10 text-gray-600"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                >
+                    <circle
+                        cx="12"
+                        cy="12"
+                        r="10"
+                        stroke="purple"
+                        strokeWidth="2"
+                    />
+                    <circle
+                        cx="12"
+                        cy="12"
+                        r="10"
+                        stroke="white"
+                        strokeWidth="2"
+                        strokeDasharray="31.4 62.8"
+                        strokeDashoffset="0"
+                    />
+                </svg>
+            )}
             <ul className="flex justify-between items-center w-full px-4">
                 <div className="flex items-center gap-x-4">
                     <li>
@@ -41,9 +83,13 @@ const ShoppingCartProducts = ({ product }) => {
                         </div>
                         <h3>{product.product_price}</h3>
                         <div className='flex items-center gap-x-2 justify-center px-2 py-1 border '>
-                            <button type="button" onClick={async () => quantityDecrement(product.product_id)}>-</button>
+                            <button className='hover:text-purple-300 disabled:text-gray-300' disabled={Loading || product.product_quantity == 1} onClick={() => quantityMinus()}>
+                                -
+                            </button>
                             <p min="1" max="10" className="w-full text-center">{product.product_quantity}</p>
-                            <button type="button" onClick={async () => quantityIncrement(product.product_id)}>+</button>
+                            <button className='hover:text-purple-300' disabled={Loading} onClick={() => quantityPlus()}>
+                                +
+                            </button>
                         </div>
                     </li>
                 </div>
@@ -60,21 +106,21 @@ const ShoppingCartProducts = ({ product }) => {
                                     cx="12"
                                     cy="12"
                                     r="10"
-                                    stroke="currentColor"
-                                    strokeWidth="4"
+                                    stroke="purple"
+                                    strokeWidth="2"
                                 />
                                 <circle
                                     cx="12"
                                     cy="12"
                                     r="10"
                                     stroke="white"
-                                    strokeWidth="4"
+                                    strokeWidth="2"
                                     strokeDasharray="31.4 62.8"
                                     strokeDashoffset="0"
                                 />
                             </svg>
                         ) : (
-                            <TbShoppingCartCancel className="size-8 hover:text-red-500" />
+                            <TbShoppingCartCancel className="size-8 hover:text-purple-400" />
                         )}
                     </button>
                 </li>
